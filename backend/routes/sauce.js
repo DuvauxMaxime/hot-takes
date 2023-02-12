b53c62060b23f5ba4a3e17a4295f3d1e97b52fb0
DuvauxMaxime <devpro.dvx@gmail.com>
2023-02-12 09:55:54 +0100

    fix(controllers & routes/sauce.js) - Error createSauce

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,9 +5,9 @@ const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
 
-router.post('/', multer, sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 router.post('/:id/like', sauceCtrl.likeSauce);
-router.put('/:id', sauceCtrl.editSauce);
+router.put('/:id', multer, sauceCtrl.editSauce);
 router.delete('/:id', sauceCtrl.deleteSauce);
 router.get('/:id', sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
