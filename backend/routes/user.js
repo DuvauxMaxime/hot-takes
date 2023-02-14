e1773f1e7d9cb468460fef4fd1452fee3bedd76b
DuvauxMaxime <devpro.dvx@gmail.com>
2023-02-14 11:38:45 +0100

    (routes/user.js, models/User.js, middleware/email-validator.js) - Modification validation email unique Signup : ajout middleware email-validator

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const limiter = require('../middleware/rate-limite');
+const emailValidator = require('../middleware/email-validator')
 const passwordValidator = require('../middleware/password-validator')
 
 
@@ -10,7 +11,7 @@ const userCtrl = require('../controllers/user')
 
 
 // ROUTES
-router.post('/signup', passwordValidator, userCtrl.signup);
+router.post('/signup', emailValidator, passwordValidator, userCtrl.signup);
 router.post('/login', limiter, userCtrl.login);
 
 
