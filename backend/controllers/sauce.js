4464f3336bb33e3d497e04b5ace2d78462fccece
DuvauxMaxime <devpro.dvx@gmail.com>
2023-02-15 10:04:12 +0100

    fix(controllers/sauce.js) - edit error message getOneSauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -83,5 +83,5 @@ exports.getAllSauces = (req, res, next) => {
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
+        .catch(error => res.status(404).json({ message: `La sauce n'existe pas !` }));
 };
