0bc4e686e960f7c0b4fe7eaf40e0d67f103415ee
DuvauxMaxime <devpro.dvx@gmail.com>
2023-02-18 15:59:12 +0100

    feat(controlles/sauce.js) - rewrite editSauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -164,26 +164,51 @@ exports.likeSauce = async (req, res, next) => {
     }
 }
 
+// // PUT Modifier une sauce
+// exports.editSauce = (req, res, next) => {
+//     const sauceObject = req.file ? {
+//         ...JSON.parse(req.body.sauce),
+//         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+//     } : { ...req.body };
+//     delete sauceObject._userId;
+//     Sauce.findOne({ _id: req.params.id })
+//         .then((sauce) => {
+//             if (sauce.userId != req.auth.userId) {
+//                 res.status(401).json({ message: 'Non autorisé' });
+//             } else {
+//                 Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+//                     .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+//                     .catch(error => res.status(401).json({ error }));
+//             }
+//         })
+//         .catch((error) => {
+//             res.status(400).json({ error });
+//         });
+// };
+
 // PUT Modifier une sauce
-exports.editSauce = (req, res, next) => {
+exports.editSauce = async (req, res, next) => {
     const sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
     delete sauceObject._userId;
-    Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-            if (sauce.userId != req.auth.userId) {
-                res.status(401).json({ message: 'Non autorisé' });
-            } else {
-                Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Objet modifié!' }))
-                    .catch(error => res.status(401).json({ error }));
+    try {
+        const sauce = await Sauce.findOne({ _id: req.params.id })
+        if (sauce.userId != req.auth.userId) {
+            res.status(401).json({ message: 'Non autorisé' });
+        } else {
+            try {
+                await Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                res.status(200).json({ message: 'Objet modifié!' })
             }
-        })
-        .catch((error) => {
+            catch { (error) => res.status(401).json({ error }) }
+        }
+    } catch {
+        ((error) => {
             res.status(400).json({ error });
-        });
+        })
+    }
 };
 
 //GET Toutes les sauces
