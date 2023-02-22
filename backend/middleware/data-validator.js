ba05a14fa9df2dc5cc29555acaacabfb568f1dad
DuvauxMaxime <devpro.dvx@gmail.com>
2023-02-22 11:31:50 +0100

    feat(middleware/data-validator.js) - ajout de la validation des fichiers img avec data-validator

--- backend/middleware/data-validator.js
+++ backend/middleware/data-validator.js
@@ -2,8 +2,17 @@
 const validator = require('validator');
 
 
+// Dictionnaire des extensions de fichier
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+};
+
+
 module.exports = (req, res, next) => {
     const data = JSON.parse(req.body.sauce)
+    console.log(req.file.filename);
     if (!validator.matches(data.name, /^[a-zA-Z0-9 -]*$/)) {
         res.status(400).json({ message: `La data name ne correspond pas au format attendu` });
     } if (!validator.matches(data.manufacturer, /^[a-zA-Z0-9 -]*$/)) {
@@ -12,6 +21,13 @@ module.exports = (req, res, next) => {
         res.status(400).json({ message: `La data description ne correspond pas au format attendu` });
     } if (!validator.matches(data.mainPepper, /^[a-zA-Z0-9 -]*$/)) {
         res.status(400).json({ message: `La data mainPepper ne correspond pas au format attendu` });
+    }
+    const extension = req.file.mimetype.split('/')[1]; // Défini l'extension d'après le dictionnaire MIME TYPES
+    const tabExtension = Object.keys(MIME_TYPES).map(cle => {
+        return MIME_TYPES[cle]
+    })
+    if (tabExtension.includes(extension) === false) {
+        res.status(400).json({ message: `Le format du fichier n'est pas pris en charge ! Seuls les fichiers jpg, jpeg ou png sont acceptés.` })
     } else {
         next();
     }
