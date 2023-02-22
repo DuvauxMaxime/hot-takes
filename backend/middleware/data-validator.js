f7ccff8c70f8ec19967f3a064c306c29142c6f2d
DuvauxMaxime <devpro.dvx@gmail.com>
2023-02-22 10:41:37 +0100

    feat(middleware/data-validator.js) - middleware pour validation des datas transmises

--- backend/middleware/data-validator.js
+++ backend/middleware/data-validator.js
@@ -4,14 +4,13 @@ const validator = require('validator');
 
 module.exports = (req, res, next) => {
     const data = JSON.parse(req.body.sauce)
-    console.log(data);
-    if (!validator.matches(data.name, /^[a-zA-Z -_]*$/)) {
+    if (!validator.matches(data.name, /^[a-zA-Z0-9 -]*$/)) {
         res.status(400).json({ message: `La data name ne correspond pas au format attendu` });
-    } if (!validator.matches(data.manufacturer, /^[a-zA-Z -_#]*$/)) {
+    } if (!validator.matches(data.manufacturer, /^[a-zA-Z0-9 -]*$/)) {
         res.status(400).json({ message: `La data manufacturer ne correspond pas au format attendu` });
-    } if (!validator.matches(data.description, /^[a-zA-Z -_#]*$/)) {
+    } if (!validator.matches(data.description, /^[a-zA-Z0-9 -]*$/)) {
         res.status(400).json({ message: `La data description ne correspond pas au format attendu` });
-    } if (!validator.matches(data.mainPepper, /^[a-zA-Z -_#]*$/)) {
+    } if (!validator.matches(data.mainPepper, /^[a-zA-Z0-9 -]*$/)) {
         res.status(400).json({ message: `La data mainPepper ne correspond pas au format attendu` });
     } else {
         next();
