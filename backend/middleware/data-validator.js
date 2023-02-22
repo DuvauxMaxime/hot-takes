7983023fa48ae13742ff7ffed67783d47c2f7d62
DuvauxMaxime <devpro.dvx@gmail.com>
2023-02-22 17:28:23 +0100

    fix(middleware/data-validator.js & controllers/sauce.js) - editSauce erreur : pb non prise en compte des modifs si pas de fichier lors de modification

--- backend/middleware/data-validator.js
+++ backend/middleware/data-validator.js
@@ -11,7 +11,12 @@ const MIME_TYPES = {
 
 
 module.exports = (req, res, next) => {
-    const data = JSON.parse(req.body.sauce)
+    let data
+    if (req.body.sauce === undefined) {
+        data = req.body
+    } else {
+        data = JSON.parse(req.body.sauce)
+    }
     if (!validator.matches(data.name.trim(), /^[a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.\-\,]*$/) || data.name.trim().length === 0) {
         return res.status(400).json({ message: `La data name ne correspond pas au format attendu. (La data doit contenir au moins 1 caractère alphanumérique)` });
     } if (!validator.matches(data.manufacturer.trim(), /^[a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.\-\,]*$/) || data.manufacturer.trim().length === 0) {
@@ -22,13 +27,15 @@ module.exports = (req, res, next) => {
         return res.status(400).json({ message: `La data mainPepper ne correspond pas au format attendu. (La data doit contenir au moins 1 caractère alphanumérique)` });
     } if (parseInt(data.heat) > 10 || parseInt(data.heat) < 1) {
         return res.status(400).json({ message: `La data heat doit être comprise entre 1 et 10. (La data ne peut être vide)` });
-    }
-    const extension = req.file.mimetype.split('/')[1]; // Défini l'extension d'après le dictionnaire MIME TYPES
-    const tabExtension = Object.keys(MIME_TYPES).map(cle => {
-        return MIME_TYPES[cle]
-    })
-    if (tabExtension.includes(extension) === false) {
-        return res.status(400).json({ message: `Le format du fichier n'est pas pris en charge ! Seuls les fichiers jpg, jpeg ou png sont acceptés.` })
+    } if (req.file != undefined) {
+        const extension = req.file.mimetype.split('/')[1]; // Défini l'extension d'après le dictionnaire MIME TYPES
+        const tabExtension = Object.keys(MIME_TYPES).map(cle => {
+            return MIME_TYPES[cle]
+        })
+        if (tabExtension.includes(extension) === false) {
+            return res.status(400).json({ message: `Le format du fichier n'est pas pris en charge ! Seuls les fichiers jpg, jpeg ou png sont acceptés.` })
+        }
+        next();
     } else {
         next();
     }
