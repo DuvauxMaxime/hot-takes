99d84218cb772502bffb034a6e3f4afd8b0aab8c
DuvauxMaxime <devpro.dvx@gmail.com>
2023-03-02 14:42:34 +0100

    clean code

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -21,21 +21,6 @@ const storage = multer.diskStorage({ //fonction pour enregistrer sur le disk
     }
 });
 
-// const storage = multer.diskStorage({ //fonction pour enregistrer sur le disk
-//     destination: (req, file, callback) => {
-//         callback(null, 'images')
-//     },
-//     filename: (req, file, callback) => {
-//         const name = file.originalname.split(' ').join('_'); // Remplace les espaces par des _ dans le nom du fichier (pour éviter erreurs serveur)
-//         const extension = MIME_TYPES[file.mimetype]; // Défini l'extension d'après le dictionnaire MIME TYPES
-//         const nameWithoutExtension = name.split(`.`)[0]; // Supprime l'extension du fichier d'origine
-//         if (extension === undefined) {
-//             callback(null, "");
-//         } else {
-//             callback(null, nameWithoutExtension + Date.now() + '.' + extension);    // Créer un nom de fichier pour son stockage avec ajout TimeStamp pour le rendre unique
-//         }
-//     }
-// });
 
 
 module.exports = multer({ storage }).single('image'); // Méthode single pour préciser que c'est un fichier image unique
